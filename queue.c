a18fd45b8aec898b07f3d54d3160c4a9a40186c7
Chisheng Chen <johnny1001s000602@gmail.com>
2025-02-23 02:34:25 +0800

    Implement function q_free
    The function q_free will free the storage
    used by the input queue.
    Change-Id: I2fa80eacadffd082ea0b92de994f62670acdd3d8

--- queue.c
+++ queue.c
@@ -24,7 +24,27 @@ struct list_head *q_new()
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *head) {}
+void q_free(struct list_head *head)
+{
+    if (!head)
+        return;
+
+    if (list_empty(head)) {
+        free(head);
+        return;
+    }
+
+    element_t *entry = NULL, *safe = NULL;
+
+    list_for_each_entry_safe (entry, safe, head, list) {
+        list_del(&entry->list);
+        if (entry->value)
+            free(entry->value);
+        free(entry);
+    }
+
+    free(head);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
